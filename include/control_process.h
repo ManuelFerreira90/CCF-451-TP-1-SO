2f5588d44bb1ab0827f65f31ab4b41274030d2c0
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2024-07-30 07:57:59 -0300

    Delete include directory

--- include/control_process.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONTROL_PROCESS_H
-#define CONTROL_PROCESS_H
-
-int start_control_process();
-
-#endif
--- include/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void handle_error(const char *message);
-
-#endif
