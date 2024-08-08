c13ba6a8c2b79a7c61decc21183ad4b2997c0335
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2024-08-08 08:14:35 -0300

    Update CPU.c

--- src/CPU.c
+++ src/CPU.c
@@ -13,13 +13,18 @@ void imprimeCPU(CPU cpu, int index)
 {
     printf("\n");
     printTableBorder();
-    printf("| %-32s %-3d |\n", "CPU Status ID", index); // Adicionado o índice aqui
+    printf("| %-32s %-3d |\n", "CPU Status ID", index); // Exibe o índice da CPU
     printTableBorder();
+    
+    const char *estadoCPU = (cpu.processoEmExecucao != NULL) ? "Ocupada" : "Vazia";
+    printf("| %-25s | %-8s |\n", "Estado CPU", estadoCPU);
+    printTableBorder();
+    
     printf("| %-25s | %8d |\n", "Contador de programa", cpu.contadorPrograma);
     printTableBorder();
     printf("| %-25s | %8d |\n", "Fatia de tempo", cpu.fatiaTempo.valor);
     printTableBorder();
-    printf("| %-36s |\n", "Memoria atual na CPU");
+    printf("| %-36s |\n", "Memória atual na CPU");
     printTableBorder();
 
     if (cpu.memoriaVect != NULL)
